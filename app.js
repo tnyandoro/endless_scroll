d99cb4bb51f390baacf121f020c335eef615a6ca
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-29 23:03:59 +0200

    Add the required courses API

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert('Welcome to JS')
+const url = 'https://script.google.com/macros/s/AKfycbxFlQXkvna9jYa';
+const val = {page:1};
+const page = {};
+
+getCourses();
+
+function getCourses() {
+  const baseURL = url + `?p=` + val.page;
+fetch(baseURL).then((rep)=> rep.json())
+.then((json)=> {
+  console.log(json.data);
+  renderPost(json.data.posts)
+})
+}
