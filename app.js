08dceac3e82e10fde811829d19bd3a683d8e3703
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-30 00:00:07 +0200

    Lets create the main DIV of the page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const url = 'https://script.google.com/macros/s/AKfycbxFlQXkvna9jYamLWhTQ1k3cJItWZDvCYoCmLTcN-CJcr7vZw/exec';
 const val = {page:1};
+// object to hold the contents
 const page = {};
- 
+page.container = document.createElement('div');
+// this is were the main contects are dumped 
+page.main = document.querySelector('section');
+page.main.append(page.container);
 getCourses();
  
 function getCourses(){
