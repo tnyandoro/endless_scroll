1a1cf40be1415c4cfcff2ab79ecd97232dd5c5e8
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-29 23:30:00 +0200

    Add the api file

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const url = 'https://script.google.com/macros/s/AKfycbxFlQXkvna9jYa';
+const url = 'https://script.google.com/macros/s/AKfycbxFlQXkvna9jYamLWhTQ1k3cJItWZDvCYoCmLTcN-CJcr7vZw/exec';
 const val = {page:1};
 const page = {};
-
+ 
 getCourses();
-
-function getCourses() {
-  const baseURL = url + `?p=` + val.page;
-fetch(baseURL).then((rep)=> rep.json())
-.then((json)=> {
-  console.log(json.data);
-  renderPost(json.data.posts)
-})
+ 
+function getCourses(){
+  const baseURL = url + '?p=' + val.page;
+  fetch(baseURL).then((rep)=> rep.json())
+  .then((json) => {
+    console.log(json.data);
+    // renderPost(json.data.posts)
+  })
 }
+
